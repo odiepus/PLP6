b01a0690dd263ebc5c19196ff8f96fbf67df2716
Hector Herrera <odiepus@gmail.com>
2017-12-04 19:04:59 -0600

    CHanged from for i in to for i in range so I can manipulate iterations. But I dont think that i'll matter after some thinking. Will complete as is for now.

--- p6.py
+++ p6.py
@@ -1,21 +1,22 @@
 import re
-str = "( + (+ 3 2) (- 7 3))"
+str = "( +  3 3)"
 
-global lastLeftParen
-lastLeftParen = 0
-global lastRightParen
-lastRightParen = 0
+global leftParenCounter
+leftParenCounter = 0
+global rightParenCounter
+rightParenCounter = 0
 
 
 class Error(Exception):
     pass
 
+
 class OperationError(Error):
     pass
 
+
 class OperandError(Error):
     pass
-
 
 
 def tokenize_str(input_str):
@@ -52,61 +53,63 @@ def prefixEval(op, op1, op2):
 
 def parseTokList(tok_list):
     global answer
-    global lastLeftParen
-    global lastRightParen
-    for i in tok_list:
-        if i is "(":
-            lastLeftParen += 1
+    global leftParenCounter
+    global rightParenCounter
+    for i in range(len(tok_list)):
+        if tok_list[i] is "(":
+            leftParenCounter += 1
             tok_list.pop(0)
             answer = parseTokList(tok_list)
-        elif i is "-":
+        if tok_list[i] is "-":
+            op = tok_list.pop(0)
+            op1 = parseTokList(tok_list)
+            tok_list.pop(0)
+            op2 = parseTokList(tok_list)
+            tok_list.pop(0)
+            prefixEval(op, op1, op2)
+        elif tok_list[i] is "+":
             op = tok_list.pop(0)
             op1 = parseTokList(tok_list)
             tok_list.pop(0)
             op2 = parseTokList(tok_list)
             tok_list.pop(0)
             return prefixEval(op, op1, op2)
-        elif i is "+":
+        elif tok_list[i] is "*":
             op = tok_list.pop(0)
             op1 = parseTokList(tok_list)
             tok_list.pop(0)
             op2 = parseTokList(tok_list)
             tok_list.pop(0)
             return prefixEval(op, op1, op2)
-        elif i is "*":
+        elif tok_list[i] is "/":
             op = tok_list.pop(0)
             op1 = parseTokList(tok_list)
             tok_list.pop(0)
             op2 = parseTokList(tok_list)
             tok_list.pop(0)
             return prefixEval(op, op1, op2)
-        elif i is "/":
+        elif tok_list[i] is "&":
             op = tok_list.pop(0)
             op1 = parseTokList(tok_list)
             tok_list.pop(0)
             op2 = parseTokList(tok_list)
             tok_list.pop(0)
             return prefixEval(op, op1, op2)
-        elif i is "&":
+        elif tok_list[i] is "|":
             op = tok_list.pop(0)
             op1 = parseTokList(tok_list)
             tok_list.pop(0)
             op2 = parseTokList(tok_list)
             tok_list.pop(0)
             return prefixEval(op, op1, op2)
-        elif i is "|":
-            op = tok_list.pop(0)
-            op1 = parseTokList(tok_list)
-            tok_list.pop(0)
-            op2 = parseTokList(tok_list)
-            tok_list.pop(0)
-            return prefixEval(op, op1, op2)
-        elif i.isdigit():
-            return i
-        elif i is ")":
-            lastRightParen += 1
-            tok_list.pop(0)
-    return answer
+        elif tok_list[i].isdigit():
+            return tok_list[i]
+        else:
+            if tok_list[i] is ")":
+                rightParenCounter += 1
+                tok_list.pop(0)
+    if leftParenCounter == rightParanCounter:
+        return answer
 
 def prefixReader(str):
     print(">", str)
@@ -125,4 +128,5 @@ def prefixReader(str):
 
 
 prefixReader(str)
+
 
